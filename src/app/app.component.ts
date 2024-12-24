b5d9402c14eac6662a3423e8687eed76639ce461
Shresth Kapoor <shresthkapoor7@gmail.com>
2024-12-24 01:14:41 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portfolio-website';
+  year = new Date().getFullYear();
+  projects = [
+    {
+      title: 'Healthy Food Analyzer',
+      description: 'An app to analyze the healthiness of food items using Gemini API.',
+      link: 'https://github.com/shresthkapoor/healthy-food-analyzer'
+    },
+    {
+      title: 'RAG for Obsidian',
+      description: 'A project to integrate RAG into Obsidian using MongoDB and Qdrant.',
+      link: 'https://github.com/shresthkapoor/rag-obsidian'
+    }
+  ];
 }
